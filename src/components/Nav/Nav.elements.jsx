6140ed02f81377cbd91eab21ce2eb9575b6dbf20
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-05 12:59:38 +0300

    feat(Nav): applied List

--- src/components/Nav/Nav.elements.jsx
+++ src/components/Nav/Nav.elements.jsx
@@ -2,12 +2,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import { DARK } from '../../style/config/colors';
 
-export const StyledNav = styled.nav`
-  display: grid;
-  grid-gap: 20px;
-  grid-auto-flow: column;
-  align-items: center;
-`;
+export const StyledNav = styled.nav``;
 
 export const NavItem = styled(Link)`
   color: ${DARK};
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,18 @@
-import { StyledNav, NavItem } from './Nav.elements';
+import { List } from '../List';
+import { NavItem } from './Nav.elements';
 
 export const Nav = () => {
+  const arr = [
+    { to: '/works', label: 'works' },
+    { to: '/blog', label: 'blog' },
+    { to: '/contacts', label: 'contacts' },
+  ];
+
+  const list = arr.map((item) => <NavItem to={item.to}>{item.label}</NavItem>);
+
   return (
-    <>
-      <StyledNav>
-        <NavItem to='/works'>works</NavItem>
-        <NavItem to='/blog'>blog</NavItem>
-        <NavItem to='/contacts'>contacts</NavItem>
-      </StyledNav>
-    </>
+    <nav>
+      <List gap='20px' list={list} />
+    </nav>
   );
 };
