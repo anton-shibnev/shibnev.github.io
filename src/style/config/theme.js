040d27da00a0698c254dc7b6ca1ba69dd22fb097
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-10 20:49:05 +0300

    fix(theme): move all to theme file

--- src/style/config/theme.js
+++ src/style/config/theme.js
@@ -1,5 +1,38 @@
-import * as _colors from './colors';
-import * as _typography from './typography';
+// colors
+export const BLACK = `#000`;
+export const WHITE = `#FFF`;
 
-export const colors = _colors
-export const typography = _typography
+// theme colors
+export const DARK = `#21243D`;
+export const PRIMARY = `#9E7682`;
+export const SECONDARY = `#4D4861`;
+export const GREY = `#F6F6FA`;
+export const BG_COLOR = WHITE;
+
+// typography
+export const FONT_FAMILY = `'Heebo', sans-serif`;
+export const FONT_SIZE = '18px';
+export const FONT_WEIGHT = '400';
+export const LINE_HEIGHT = '1.2';
+export const COLOR = DARK;
+
+// animation
+export const TRANSITION = '0.2s ease-in-out';
+
+// breakpoints
+export const BREAKPOINTS = {
+  mob: '10px',
+  xs: '768px',
+  md: '1366px',
+  lg: '1920px',
+};
+
+// container
+export const CONTAINER_MAX_WIDTH = `858px`;
+
+export const CONTAINER_PADDINGS = {
+  mob: '10px',
+  xs: '20px',
+  md: '30px',
+  lg: '40px',
+};
