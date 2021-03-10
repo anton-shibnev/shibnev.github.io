53bd3ee97ce810d4076cbc33bc07e752c66c3ba4
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-10 20:45:13 +0300

    feat(Footer): add soc list

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import { StyledFooter, FooterContainer, SocLink } from './Footer.elements';
-import { FaFacebookSquare } from 'react-icons/fa';
+import { FaFacebookSquare, FaLinkedin } from 'react-icons/fa';
+import { SiCodewars } from 'react-icons/si';
+import { List } from '../List';
+import { HhIcon } from '../icons/HhIcon';
 
 export const Footer = () => {
+  const arr = [
+    {
+      href: '#',
+      icon: FaFacebookSquare,
+    },
+    {
+      href: '#',
+      icon: SiCodewars,
+    },
+    {
+      href: '#',
+      icon: FaLinkedin,
+    },
+    {
+      href: '#',
+      icon: HhIcon,
+    },
+  ];
+
+  const list = arr.map((item) => {
+    const Item = item.icon;
+
+    return (
+      <SocLink href={item.href}>
+        <Item />
+      </SocLink>
+    );
+  });
+
   return (
     <StyledFooter>
       <FooterContainer>
-        <SocLink>
-          <FaFacebookSquare />
-        </SocLink>
+        <List gap='30' list={list}></List>
       </FooterContainer>
     </StyledFooter>
   );
