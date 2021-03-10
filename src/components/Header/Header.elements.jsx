88af77bd9e7e5482d7476664abc7fafc9d95d246
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-10 20:46:00 +0300

    fix(Header): change logo to NavLink

--- src/components/Header/Header.elements.jsx
+++ src/components/Header/Header.elements.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components/macro';
 import { Container } from '../Container';
 
@@ -12,10 +12,16 @@ export const HeaderContainer = styled(Container)`
   align-items: center;
 `;
 
-export const HeaderLogo = styled(Link)`
+export const HeaderLogo = styled(NavLink)`
   border-radius: 50%;
   background-color: #000;
   width: 3rem;
   height: 3rem;
   margin-right: auto;
+  flex-shrink: 0;
+  transition: ${(props) => props.theme.TRANSITION};
+
+  &.active {
+    background-color: ${(props) => props.theme.PRIMARY};
+  }
 `;
