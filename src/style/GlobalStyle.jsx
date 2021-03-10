ec68b107ff696521c7287fe7229dcd09d1584400
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-10 20:48:35 +0300

    fix(GlobalStyle): change to jsx

---
+++ src/style/GlobalStyle.jsx
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize};
+
+  * {
+    box-sizing: border-box;
+
+    &::before,
+    &::after {
+      box-sizing: border-box;
+    }
+  }
+
+  html {
+    color: ${(props) => props.theme.COLOR};
+    font-size: ${(props) => props.theme.FONT_SIZE};
+    font-family: ${(props) => props.theme.FONT_FAMILY};
+    line-height: ${(props) => props.theme.LINE_HEIGHT};
+    font-weight: ${(props) => props.theme.FONT_WEIGHT};
+  }
+
+  #root {
+    ${'' /* min-height: 100vh; */}
+    display: grid;
+    align-content: start;
+    grid-template-rows: auto 1fr auto;
+    background-color: ${(props) => props.theme.BG_COLOR};
+  }
+`;
