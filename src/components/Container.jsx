afc4276ca9e79527aaef6ea5f21fd4e49210ee3c
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-10 20:43:47 +0300

    fix(Container): clean code

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,15 +1,14 @@
 import styled from 'styled-components/macro';
 import marginX from '../style/modal/margin';
 import { paddingX } from '../style/modal/padding';
-import {
-  CONTAINER_PADDINGS,
-  CONTAINER_MAX_WIDTH,
-} from '../style/config/container';
+import { CONTAINER_PADDINGS, CONTAINER_MAX_WIDTH } from '../style/config/theme';
+
+const containerMaxWidth =
+  parseInt(CONTAINER_MAX_WIDTH) + parseInt(CONTAINER_PADDINGS.mob) * 2;
 
 export const Container = styled.div`
   ${marginX()};
   ${paddingX(CONTAINER_PADDINGS)};
   width: 100%;
-  max-width: ${parseInt(CONTAINER_MAX_WIDTH) +
-  parseInt(CONTAINER_PADDINGS.mob) * 2}px;
+  max-width: ${containerMaxWidth}px;
 `;
