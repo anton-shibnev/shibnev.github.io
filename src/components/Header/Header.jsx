29a9a911d71e7f37fc5b61addbfabc51057c26bb
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-10 20:46:36 +0300

    feat(Header): add burger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+// import { Burger } from '../Burger';
 import { Nav } from '../Nav/Nav';
 import { StyledHeader, HeaderContainer, HeaderLogo } from './Header.elements';
 
@@ -5,8 +6,9 @@ export const Header = () => {
   return (
     <StyledHeader>
       <HeaderContainer>
-        <HeaderLogo to='/' />
+        <HeaderLogo to='/' exact={true} />
         <Nav />
+        {/* <Burger /> */}
       </HeaderContainer>
     </StyledHeader>
   );
