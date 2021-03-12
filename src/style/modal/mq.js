a98bc26f2b6846c16105abe8498ec0895c2a1940
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-12 11:03:33 +0300

    feat(mq): update

--- src/style/modal/mq.js
+++ src/style/modal/mq.js
@@ -1,17 +1,32 @@
 import { BREAKPOINTS } from '../config/theme';
+import { rem } from './rem';
 
-export const mq = (styleKey, obj) => {
+export const mq = (styleKey, thing, toRem = true) => {
   let str = ``;
 
-  for (const key in obj) {
-    let styleValue = obj[key];
-    let breakPointsValue = BREAKPOINTS[key];
+  const init = (styleKey, styleValue) => {
+    return `${styleKey}: ${toRem ? rem(styleValue) : styleValue};`;
+  };
 
-    if (!key) continue;
+  if (typeof thing === 'object') {
+    for (const key in thing) {
+      let styleValue = thing[key];
+      let breakPointsValue = BREAKPOINTS[key];
+      let firstKey = Object.keys(BREAKPOINTS)[0];
 
-    str += `@media only screen and (min-width: ${breakPointsValue}) {
-      ${styleKey}: ${styleValue};
-    }`;
+      if (!key) continue;
+
+      if (key === firstKey) {
+        str += init(styleKey, styleValue);
+      } else {
+        str += `@media only screen and (min-width: ${breakPointsValue}) {
+       ${init(styleKey, styleValue)};
+      }`;
+      }
+    }
+  } else {
+    str += init(styleKey, thing);
   }
+
   return str;
 };
