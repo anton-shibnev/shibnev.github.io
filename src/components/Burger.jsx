57f04698b664cd4d97673c2a23e2e86f0de00cd5
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-12 11:07:25 +0300

    feat(Burger): add styles

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
+import styled from 'styled-components/macro';
+import { BtnReset } from '../style/helpers/btnReset';
+import { mq } from '../style/modal/mq';
+import { rem } from '../style/modal/rem';
+
+const StyledBurger = styled(BtnReset)`
+  ${mq('display', { mob: 'grid', xs: 'none' })};
+  grid-gap: 9px;
+  width: 30px;
+`;
+
+const BurgerLine = styled.span`
+  height: ${rem('2px')};
+  width: 100%;
+  background-color: ${(props) => props.theme.DARK};
+  display: block;
+`;
 
 export const Burger = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
 
   return (
-    <button onClick={handleClick}>
-      <span className='line'></span>
-      <span className='line'></span>
-      <span className='line'></span>
-    </button>
+    <StyledBurger onClick={handleClick} className={click ? 'active' : ''}>
+      {[...Array(3)].map((_, index) => (
+        <BurgerLine key={index} />
+      ))}
+    </StyledBurger>
   );
 };
