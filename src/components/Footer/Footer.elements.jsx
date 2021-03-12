dfcb2391e01a55ae92aee28cfcd07a0174882a09
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-12 11:07:49 +0300

    fix(Footer.elemets): clean code

--- src/components/Footer/Footer.elements.jsx
+++ src/components/Footer/Footer.elements.jsx
@@ -1,20 +1,23 @@
 import styled from 'styled-components/macro';
-import { Container } from '../Container';
+import { hoverFocus } from '../../style/modal/hoverFocus';
+import { transition } from '../../style/modal/transition';
+import { Outer } from '../Outer';
 
-export const FooterContainer = styled(Container)``;
-export const StyledFooter = styled.footer``;
+export const StyledFooter = styled(Outer)``;
 
 const StyledSocLink = styled.a`
   color: ${(props) => props.theme.DARK};
   font-size: 2rem;
+  ${transition('color')};
+  ${hoverFocus`
+    color: ${(props) => props.theme.PRIMARY};
+  `};
 `;
 
 export const SocLink = ({ href = '#', children }) => {
   return (
-    <>
-      <StyledSocLink target='__blank' href={href}>
-        {children}
-      </StyledSocLink>
-    </>
+    <StyledSocLink target='__blank' href={href}>
+      {children}
+    </StyledSocLink>
   );
 };
