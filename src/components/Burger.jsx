222e29629596ef828e149c0ff58c35fe90680052
Anton Shibneb <shibnev.anton@gmail.com>
2021-03-21 21:55:41 +0300

    feat(Burger): add animation

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -8,6 +8,7 @@ const StyledBurger = styled(BtnReset)`
   width: ${rem('35px')};
   ${transition('color')};
   color: ${(props) => (props.click ? props.theme.PRIMARY : props.theme.DARK)};
+
   ${hoverFocus`
      color: ${(props) => props.theme.PRIMARY};
  `};
@@ -19,13 +20,29 @@ const BurgerLine = styled.span`
   width: 100%;
   background-color: currentColor;
   display: block;
+  ${transition('opacity', 'transform')};
+
+  &:nth-child(1) {
+    transform: ${({ click }) =>
+      click ? 'rotate(45deg) translate(25%, 100%)' : 'rotate(0)'};
+  }
+
+  &:nth-child(2) {
+    transform: translateX(${({ click }) => (click ? '-50%' : '0')});
+    opacity: ${({ click }) => (click ? '0' : '1')};
+  }
+
+  &:nth-child(3) {
+    transform: ${({ click }) =>
+      click ? 'rotate(-45deg) translate(25%, -100%)' : 'rotate(0)'};
+  }
 `;
 
 export const Burger = ({ className, onClick, click }) => {
   return (
     <StyledBurger className={className} onClick={onClick} click={click}>
       {[...Array(3)].map((_, index) => (
-        <BurgerLine key={index} />
+        <BurgerLine click={click} key={index} />
       ))}
     </StyledBurger>
   );
