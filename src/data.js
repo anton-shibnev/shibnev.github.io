bb4665ff3d979c24f8052a8d23088a9d29eb4cd0
Anton Shibneb <shibnev.anton@gmail.com>
2021-04-09 16:32:27 +0300

    fix: data

--- src/data.js
+++ src/data.js
@@ -63,19 +63,19 @@ export const WORKS_DATA = {
       stack: ['JS', 'CSS', 'HTML', 'locale Storage'],
     },
     {
-      title: 'memory game',
-      desc:
-        'Simple game of finding pairs. Here I learned how to use the localeStorage. I am planing to rewrite to react',
-      gitHref: 'https://github.com/shibnev/pairs',
-      webHref: 'https://shibnev.github.io/pairs/index.html',
-      stack: ['JS', 'CSS', 'HTML', 'locale Storage'],
+      title: 'fab store',
+      desc: 'Shoe store. I am planing to rewrite to react',
+      gitHref: 'https://github.com/shibnev/shoes-store',
+      webHref: 'https://shibnev.github.io/shoes-store/dist/index.html',
+      stack: ['JS', 'SCSS', 'HTML', 'PUG', 'BEM'],
     },
     {
       title: 'Students Table',
-      desc: 'Input form, filter and table with sort, for add new students',
-      gitHref: 'https://github.com/shibnev/students-table',
-      webHref: 'https://shibnev.github.io/students-table/',
-      stack: ['JS', 'Bootstrap', 'HTML', 'locale Storage'],
+      desc:
+        'Input form, filter and table with sort, for add new students',
+      gitHref: 'https://github.com/shibnev/pairs',
+      webHref: 'https://shibnev.github.io/pairs/index.html',
+      stack: ['JS', 'Bootstrap', 'js class', 'locale Storage'],
     },
   ],
 };
